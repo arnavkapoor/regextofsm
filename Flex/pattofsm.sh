b59e2b136450ec7ce0b738d1d540a2d74f558e44
Vanya Yaneva <wyaneva@gmail.com>
2018-06-29 13:40:55 +0100

    Saving the regex into the file too

--- Flex/pattofsm.sh
+++ Flex/pattofsm.sh
@@ -14,7 +14,9 @@ do
   echo "%%" >> temp.txt
   flex -v -T temp.txt 2> temp2.txt  # running the flex tool 
   python flextokiss2format.py temp2.txt > temp3.txt # python script to create kiss2 format output from flex output.
-  cat temp3.txt > "$FSM_DIRECTORY/$filename.fsm" # saving the fsm files
+
+  cat $f > "$FSM_DIRECTORY/$filename.fsm" # saving the fsm files
+  cat temp3.txt >> "$FSM_DIRECTORY/$filename.fsm" # saving the fsm files
   rm temp.txt
   rm temp2.txt
   rm temp3.txt
