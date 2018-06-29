11c2683e2542ca0e79687cf3d1868a8e95f5be6d
Vanya Yaneva <wyaneva@gmail.com>
2018-06-29 09:44:29 +0100

    Script to generate FSMs takes src and dest dirs
      as input parameters

---
+++ Flex/pattofsm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PATTERN_FILES=$1/* # directory containing the regex patterns
+FSM_DIRECTORY=$2 # directory to save the fsms
+
+for f in $PATTERN_FILES
+do
+  filename=$(basename $f)
+
+  cat $f
+   
+  echo "%%" >> temp.txt
+  cat $f >> temp.txt 
+  echo "%%" >> temp.txt
+  flex -v -T temp.txt 2> temp2.txt  # running the flex tool 
+  python flextokiss2format.py temp2.txt > temp3.txt # python script to create kiss2 format output from flex output.
+  cat temp3.txt > "$FSM_DIRECTORY/$filename.fsm" # saving the fsm files
+  rm temp.txt
+  rm temp2.txt
+  rm temp3.txt
+done
